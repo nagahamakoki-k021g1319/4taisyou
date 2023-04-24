fe77a9be034428cc080fad2f3964d2586c21b294
一真 田代 <t.kazuma.0809@gmail.com>
2023-04-24 17:30:24 +0900

    敵の攻撃速度の調整

--- User/EnemyExplosionAttack.h
+++ User/EnemyExplosionAttack.h
@@ -1,4 +1,7 @@
 #pragma once
+#include "Object3d.h"
+#include "Collision.h"
+
 class EnemyExplosionAttack
 {
 public:
@@ -11,6 +14,25 @@ public:
 
 	void Explosion();
 
-private:
+	void OnCollision();
 
+	void PlayerVec();
+
+private:
+	Collision* coll = nullptr;
+	Object3d* obj_ = nullptr;
+
+	Object3d* attackRenge = nullptr;
+
+	Vector3 playerLen;
+	Vector3 playerPos;
+
+	float enemySpeed = 0.5f;
+
+	bool attackHit = false;
+	bool isAttack = false;
+	int isAttackTime = 10;
+	int attackAccumulate = 50;
+	bool hitAttck = false;
+	bool hitOne = false;
 };
